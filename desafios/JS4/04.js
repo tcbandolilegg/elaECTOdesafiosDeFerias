dcd648292e8425d777175dbdf66a19a44329b7f3
Thereza Bandoli <tcbandolilegg@gmail.com>
2020-12-23 18:51:42 -0300

    colocando os desafios m um menu, e  também em uma galeria de escolha com imagens

---
+++ desafios/JS4/04.js
@@ -0,0 +1,9 @@
+// DESAFIO 4:
+
+// Crie um programa que receba um número inteiro e imprima na tela seu sucessor e antecessor. A resposta deve ser: "Analisando o número x, seu antecessor é y e seu sucessor é z".
+alert("SOLUÇÃO ANTECESSOR E SUCESSOR")
+let num = Number(prompt("Digite um número"))
+let sucessor = num + 1
+let antecessor = num - 1
+
+console.log("O sucessor de ", num, " é ", sucessor, " seu antecesor é ", antecessor)
